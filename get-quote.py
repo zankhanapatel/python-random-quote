3fd85a9584d1828791681bfc2f5f747bc53c85af
ZankhanaPatel <zankhana810@ymail.com>
2022-03-31 15:30:11 -0700

    completed
    completed

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,10 @@
 import random
 def primary():
   print("Keep it logically awesome.")
+  
+  f = open("quotes.txt", "a")
+  f.write("\nWoops! I have deleted the content!")
+  f.close()
 
   f = open("quotes.txt")
   quotes = f.readlines()
@@ -8,6 +12,7 @@ def primary():
   last = len(quotes) - 1
   rnd = random.randint(0, last)
   
+  print(quotes[rnd])
   print(quotes[rnd])
 
 if __name__== "__main__":
